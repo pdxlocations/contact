aa736472cc9e659450ce822e91e36b94c07aede1
pdxlocations <benlipsey@gmail.com>
2025-02-08 18:02:49 -0800

    fix conflict

--- main.py
+++ main.py
@@ -11,10 +11,7 @@ from pubsub import pub
 import os
 import logging
 import traceback
-<<<<<<< HEAD
-=======
 import threading
->>>>>>> cd74a32 (try a threading lock)
 
 from utilities.arg_parser import setup_parser
 from utilities.interfaces import initialize_interface
@@ -53,7 +50,7 @@ def main(stdscr):
         with lock: 
             globals.interface = initialize_interface(args)
         logging.info("Interface initialized")
-        
+
         globals.myNodeNum = get_nodeNum()
         globals.channel_list = get_channels()
         globals.node_list = get_node_list()
